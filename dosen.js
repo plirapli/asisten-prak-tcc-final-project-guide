feb9b2d055e31a4dec5a6fb3da768f708b2ce945
plirapli <rafliyulistiawan@gmail.com>
2024-05-19 21:16:09 +0700

    add fe

---
+++ dosen.js
@@ -0,0 +1,133 @@
+// Ngambil elemen form
+const formulir = document.querySelector("form");
+
+// // Bikin trigger event submit pada elemen form
+formulir.addEventListener("submit", (e) => {
+  e.preventDefault();
+  kirim();
+});
+
+function kirim() {
+  // Ngambil elemen input
+  const elemen_nama = document.querySelector("#nama");
+  const elemen_nidn = document.querySelector("#nidn");
+
+  // Ngambil value (nidn) dari elemen input
+  const id = elemen_nama.dataset.id;
+  const nama = elemen_nama.value;
+  const nidn = elemen_nidn.value;
+
+  // Ngecek apakah harus POST atau PUT
+  if (id == "") {
+    // Tambah catatan
+    axios
+      .post("http://localhost:3100/dosen", {
+        nama,
+        nidn,
+      })
+      .then(() => {
+        // bersihin formnya
+        elemen_nama.dataset.id = "";
+        elemen_nama.value = "";
+        elemen_nidn.value = "";
+
+        // manggil fungsi get catatan biar datanya di-refresh
+        getCatatan();
+      })
+      .catch((error) => console.log(error.message));
+  } else {
+    axios
+      .put(`http://localhost:3100/dosen/${id}`, {
+        nama,
+        nidn,
+      })
+      .then(() => {
+        // bersihin formnya
+        elemen_nama.dataset.id = "";
+        elemen_nama.value = "";
+        elemen_nidn.value = "";
+
+        // manggil fungsi get catatan biar datanya direfresh
+        getCatatan();
+      })
+      .catch((error) => console.log(error));
+  }
+}
+
+// Ngambil catatan
+function getCatatan() {
+  axios
+    .get("http://localhost:3100/dosen")
+    .then(({ data }) => {
+      const table = document.querySelector("#table-dosen");
+      const { data: dosen } = data;
+      let tampilan = "";
+      let no = 1;
+
+      for (const dos of dosen) {
+        tampilan += tampilkanCatatan(no, dos);
+        no++;
+      }
+      table.innerHTML = tampilan;
+
+      hapusCatatan();
+      editCatatan();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+function tampilkanCatatan(no, dosen) {
+  return `
+    <tr>
+      <td>${no}</td>
+      <td class="nama">${dosen.nama}</td>
+      <td class="nidn">${dosen.nidn}</td>
+      <td><button data-id=${dosen.id} class='btn-edit'>Edit</button></td>
+      <td><button data-id=${dosen.id} class='btn-hapus'>Hapus</button></td>
+    </tr>
+  `;
+}
+
+function hapusCatatan() {
+  const kumpulan_tombol_hapus = document.querySelectorAll(".btn-hapus");
+
+  kumpulan_tombol_hapus.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const id = btn.dataset.id;
+      axios
+        .delete(`http://localhost:3100/dosen/${id}`)
+        .then(() => getCatatan())
+        .catch((error) => console.log(error));
+    });
+  });
+}
+
+function editCatatan() {
+  const kumpulan_tombol_edit = document.querySelectorAll(".btn-edit");
+
+  kumpulan_tombol_edit.forEach((tombol_edit) => {
+    tombol_edit.addEventListener("click", () => {
+      const id = tombol_edit.dataset.id;
+      const nama =
+        tombol_edit.parentElement.parentElement.querySelector(
+          ".nama"
+        ).innerText;
+      const nidn =
+        tombol_edit.parentElement.parentElement.querySelector(
+          ".nidn"
+        ).innerText;
+
+      // Ngambil elemen input
+      const elemen_nama = document.querySelector("#nama");
+      const elemen_nidn = document.querySelector("#nidn");
+
+      elemen_nama.dataset.id = id;
+      elemen_nama.value = nama;
+      elemen_nidn.value = nidn;
+    });
+  });
+}
+
+getCatatan();
---
+++ mhs.js
@@ -0,0 +1,131 @@
+// Ngambil elemen form
+const formulir = document.querySelector("form");
+
+// // Bikin trigger event submit pada elemen form
+formulir.addEventListener("submit", (e) => {
+  e.preventDefault();
+  kirim();
+});
+
+function kirim() {
+  // Ngambil elemen input
+  const elemen_nama = document.querySelector("#nama");
+  const elemen_nim = document.querySelector("#nim");
+
+  // Ngambil value (nim) dari elemen input
+  const id = elemen_nama.dataset.id;
+  const nama = elemen_nama.value;
+  const nim = elemen_nim.value;
+
+  // Ngecek apakah harus POST atau PUT
+  if (id == "") {
+    // Tambah catatan
+    axios
+      .post("http://localhost:3100/mahasiswa", {
+        nama,
+        nim,
+      })
+      .then(() => {
+        // bersihin formnya
+        elemen_nama.dataset.id = "";
+        elemen_nama.value = "";
+        elemen_nim.value = "";
+
+        // manggil fungsi get catatan biar datanya di-refresh
+        getCatatan();
+      })
+      .catch((error) => console.log(error.message));
+  } else {
+    axios
+      .put(`http://localhost:3100/mahasiswa/${id}`, {
+        nama,
+        nim,
+      })
+      .then(() => {
+        // bersihin formnya
+        elemen_nama.dataset.id = "";
+        elemen_nama.value = "";
+        elemen_nim.value = "";
+
+        // manggil fungsi get catatan biar datanya direfresh
+        getCatatan();
+      })
+      .catch((error) => console.log(error));
+  }
+}
+
+// Ngambil catatan
+function getCatatan() {
+  axios
+    .get("http://localhost:3100/mahasiswa")
+    .then(({ data }) => {
+      const table = document.querySelector("#table-mhs");
+      const { data: mahasiswa } = data;
+      let tampilan = "";
+      let no = 1;
+
+      for (const mhs of mahasiswa) {
+        tampilan += tampilkanCatatan(no, mhs);
+        no++;
+      }
+      table.innerHTML = tampilan;
+
+      hapusCatatan();
+      editCatatan();
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+}
+
+function tampilkanCatatan(no, mhs) {
+  return `
+    <tr>
+      <td>${no}</td>
+      <td class="nama">${mhs.nama}</td>
+      <td class="nim">${mhs.nim}</td>
+      <td><button data-id=${mhs.id} class='btn-edit'>Edit</button></td>
+      <td><button data-id=${mhs.id} class='btn-hapus'>Hapus</button></td>
+    </tr>
+  `;
+}
+
+function hapusCatatan() {
+  const kumpulan_tombol_hapus = document.querySelectorAll(".btn-hapus");
+
+  kumpulan_tombol_hapus.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const id = btn.dataset.id;
+      axios
+        .delete(`http://localhost:3100/mahasiswa/${id}`)
+        .then(() => getCatatan())
+        .catch((error) => console.log(error));
+    });
+  });
+}
+
+function editCatatan() {
+  const kumpulan_tombol_edit = document.querySelectorAll(".btn-edit");
+
+  kumpulan_tombol_edit.forEach((tombol_edit) => {
+    tombol_edit.addEventListener("click", () => {
+      const id = tombol_edit.dataset.id;
+      const nama =
+        tombol_edit.parentElement.parentElement.querySelector(
+          ".nama"
+        ).innerText;
+      const nim =
+        tombol_edit.parentElement.parentElement.querySelector(".nim").innerText;
+
+      // Ngambil elemen input
+      const elemen_nama = document.querySelector("#nama");
+      const elemen_nim = document.querySelector("#nim");
+
+      elemen_nama.dataset.id = id;
+      elemen_nama.value = nama;
+      elemen_nim.value = nim;
+    });
+  });
+}
+
+getCatatan();
